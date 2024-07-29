1b862b39f17bfebd31df46c91d9703be4dfa1f6f
DhanaSelvan J U <93440351+DhanaSelvan@users.noreply.github.com>
2024-07-29 16:35:55 +0530

    update this repo

--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@ var input = document.getElementById('input')
 setInterval(() => {
     var value = input.value.trim();
     value = value.replaceAll(" ", "")
-    document.getElementById('value').innerHTML = value.length;
+    var length = value.length;
+    var rem = 50-length;
+    document.getElementById('value').innerHTML = length;
+    document.getElementById('rem').innerHTML = rem;
+    if(rem<0){
+        alert("Yo reached the limit");
+    }
 }, 500);
